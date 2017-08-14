6354ef5379bfd90cd7a01d1ec2c93b0de9908733
Britney Wilbourn <bwilbourn@basaecampcodingacademy.org>
2017-08-14 14:12:52 -0500

    I made 4 functions in core.py -> new_gladiator, is_dead, heal, and attack... now I am figuring out how shell.py should be set up. and I still have to make test functions.

---
+++ core.py
@@ -0,0 +1,36 @@
+import random
+
+
+def new_gladiator(health, rage, damage_low, damage_high):
+    """dict -> dict"""
+    return {
+        'health': health,
+        'rage': rage,
+        'damage_low': damage_low,
+        'damage_high': damage_high
+    }
+
+
+def is_dead(gladiator):
+    if gladiator['health'] <= 0:
+        return True
+    else:
+        return False
+
+
+def heal(gladiator):
+    if gladiator['rage'] >= 10:
+        gladiator['rage'] = max(gladiator['rage'] - 10, 0)
+        gladiator['health'] = min(gladiator['health'] + 5, 100)
+        return gladiator
+
+
+def attack(attacker, defender):
+    damage_dealt = randint(attacker['damage_low'], attacker['damage_high'])
+    if randint(1, 100) <= attacker['rage']:
+        defender['health'] = damage_dealt * 2
+        attacker['rage'] = 0
+    else:
+        defender['health'] -= damage_dealt
+        attacker['rage'] += 15
+    return attacker, defender
---
+++ shell.py
@@ -0,0 +1,9 @@
+import core
+
+
+def main():
+    gladiator = core.new_gladiator(100, 0, 10, 20)
+
+
+if __name__ == '__main__':
+    main()
