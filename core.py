d9b476a98cd3e26c1f81097853d9246e3fd51787
Britney Wilbourn <bwilbourn@basaecampcodingacademy.org>
2017-08-14 16:21:35 -0500

    Gladiators work, attack and quit work also but now I have to fix heal....

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 
 
 def new_gladiator(health, rage, damage_low, damage_high):
--- shell.py
+++ shell.py
@@ -2,7 +2,47 @@ import core
 
 
 def main():
-    gladiator = core.new_gladiator(100, 0, 10, 20)
+    print('Prepare for total domination!!')
+    gladiator_1 = core.new_gladiator(100, 0, 10, 20)
+    gladiator_2 = core.new_gladiator(100, 0, 10, 20)
+    while True:
+        choice = input("""Gladiator1, what's your move?\n
+        \t-heal
+        \t-attack
+        \t-pass
+        \t-quit\n""")
+        if choice == 'attack':
+            core.attack(gladiator_1, gladiator_2)
+            if core.is_dead(gladiator_2):
+                print('Gladiator2 is dead, Gladiator1 wins.')
+                break
+        elif choice == 'heal':
+            core.heal(gladiator)
+        elif choice == 'pass':
+            print('pass')
+        elif choice == 'quit':
+            exit()
+        print('gladiator_1:', gladiator_1)
+        print('gladiator_2:', gladiator_2)
+
+        choice = input("""Gladiator2, what's your move?\n
+        \t-heal
+        \t-attack
+        \t-pass
+        \t-quit\n""")
+        if choice == 'attack':
+            core.attack(gladiator_2, gladiator_1)
+            if core.is_dead(gladiator_1):
+                print('Gladiator1 is dead, Gladiator2 wins.')
+                break
+        elif choice == 'heal':
+            core.heal(gladiator)
+        elif choice == 'pass':
+            print('pass')
+        elif choice == 'quit':
+            exit()
+        print('gladiator_1:', gladiator_1)
+        print('gladiator_2:', gladiator_2)
 
 
 if __name__ == '__main__':
