cf82b2a0c2360d3f805f752b40ceebc8f215ed8b
Britney Wilbourn <bwilbourn@basaecampcodingacademy.org>
2017-08-15 11:38:13 -0500

    I finished everything. I have all the functions for core.py and my shell.py works and runs. I made test functions in test_core.py and they all run. Now I have 100% test coverage.

--- test_core.py
+++ test_core.py
@@ -13,7 +13,23 @@ def test_new_gladiator():
 def test_is_dead():
     choice = {'rage': 15, 'damage_high': 20, 'health': -1, 'damage_low': 10}
     assert core.is_dead(choice) == True
+    choice = {'rage': 15, 'damage_high': 20, 'health': 20, 'damage_low': 10}
+    assert core.is_dead(choice) == False
 
 
 def test_heal():
-    assert core.heal(gladiator)
+    choice = {'rage': 15, 'damage_high': 20, 'health': 100, 'damage_low': 10}
+    assert core.heal(choice) == choice
+
+
+def test_attack():
+    attacker = {'health': 75, 'rage': 0, 'damage_low': 10, 'damage_high': 10}
+    defender = {'health': 75, 'rage': 0, 'damage_low': 11, 'damage_high': 13}
+    core.attack(attacker, defender)
+    assert attacker['rage'] == 15
+    assert defender['health'] == 65
+    attacker = {'health': 75, 'rage': 100, 'damage_low': 10, 'damage_high': 10}
+    defender = {'health': 65, 'rage': 100, 'damage_low': 11, 'damage_high': 13}
+    core.attack(attacker, defender)
+    assert attacker['rage'] == 0
+    assert defender['health'] == 20
